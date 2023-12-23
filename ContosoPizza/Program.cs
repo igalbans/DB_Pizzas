f525be8b58c6b432fe8b971c38aba63699b6a755
Laurita <Laurita@DESKTOP-NRRS5DT>
2023-12-23 12:43:46 -0500

    Add project files.

---
+++ ContosoPizza/Program.cs
@@ -0,0 +1,42 @@
+using ContosoPizza.Services;
+// Additional using declarations
+using ContosoPizza.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add the PizzaContext
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContossoPizza.db");
+
+// Add the PromotionsContext
+
+builder.Services.AddScoped<PizzaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add the CreateDbIfNotExists method call
+
+app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
+
+
+app.Run();
---
+++ ContosoPizza/Service/PizzaService.cs
@@ -0,0 +1,46 @@
+using ContosoPizza.Models;
+using ContosoPizza.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace ContosoPizza.Services;
+
+public class PizzaService
+{
+    private readonly PizzaContext _context;
+    public PizzaService(PizzaContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Pizza> GetAll()
+    {
+        return _context.Pizzas.AsNoTracking().ToList();
+    }
+
+    public Pizza? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Pizza? Create(Pizza newPizza)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddTopping(int PizzaId, int ToppingId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateSauce(int PizzaId, int SauceId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteById(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
