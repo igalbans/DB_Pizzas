ea33a7f1cd77b4a9a2868afd0f78525bb59b0840
igalbans <iran.galban.smith@gmail.com>
2023-12-26 10:33:41 -0500

    add method CreateDbIfNotExists() to Programs.cs

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -35,6 +35,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Add the CreateDbIfNotExists method call
+app.CreateDbIfNotExists();
 
 app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
 
